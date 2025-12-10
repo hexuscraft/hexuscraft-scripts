860d97a41ad2bf172f01f370fab6bb915e40a8ff
Render <27890173+itsrenderman@users.noreply.github.com>
2025-12-10 13:09:51 +0000

    refactor: improved kill flow

--- killServer.cmd
+++ killServer.cmd
@@ -2,20 +2,19 @@
 goto :main
 
 :log
-	>>"killServer.log" echo [%date%] [%time%] %~1
+	>>"servers.log" echo [%date%] [%time%] %~1
 	exit /b 0
 
 :main
 	set serverName=%1
 	call :log "Killing %serverName%"
 
+	:: We run "taskkill" twice as ServerMonitor *could* attempt to start the same server while it's already running.
+	:: To be clear: this should not happen, but this is a fallback just in-case it does.
 	taskkill /f /fi "WINDOWTITLE eq %serverName%"
 	taskkill /f /fi "WINDOWTITLE eq %serverName%"
-	taskkill /f /fi "WINDOWTITLE eq %serverName%"
-	taskkill /f /fi "WINDOWTITLE eq %serverName%"
-	taskkill /f /fi "WINDOWTITLE eq %serverName%"
+
 	rmdir /S /Q "%USERPROFILE%\Minecraft\Servers\%serverName%" > nul
 
 	call :log "Killed %serverName%"
-	echo Success
-	exit
+	exit /b 0
