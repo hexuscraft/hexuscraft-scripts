25ce079f9d0d25f83f94ee35688cd27e40b89b54
Render <27890173+itsrenderman@users.noreply.github.com>
2025-12-27 09:09:02 +0000

    refactor: prevent directory removal for 'Clans' server group

--- killServer.cmd
+++ killServer.cmd
@@ -7,14 +7,17 @@ goto :main
 
 :main
 	set serverName=%1
-	call :log "Killing %serverName%"
+	set serverGroup=%2
+	call :log "Killing %serverName% (%serverGroup%)"
 
 	:: We run "taskkill" twice as ServerMonitor *could* attempt to start the same server while it's already running.
 	:: To be clear: this should not happen, but this is a fallback just in-case it does.
 	taskkill /f /fi "WINDOWTITLE eq %serverName%"
 	taskkill /f /fi "WINDOWTITLE eq %serverName%"
 
-	rmdir /S /Q "%USERPROFILE%\Minecraft\Servers\%serverName%" > nul
+	if NOT "%serverGroup%"=="Clans" (
+		rmdir /S /Q "%USERPROFILE%\Minecraft\Servers\%serverName%" > nul
+	)
 
 	call :log "Killed %serverName%"
 	exit /b 0
